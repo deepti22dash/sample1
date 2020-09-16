8b23f351c872c93a3c3c58631dfd771c9fbbd57d
Deepti <deeptidash22@gmail.com>
2020-09-16 18:25:34 +0530

    commit 1

---
+++ calculator1001/calculator1001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using calculator100;
+
+namespace calculator1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Functions.Addition(4, 6));
+            Console.WriteLine(Functions.Subtraction(8, 5));
+            Console.WriteLine(Functions.Multiplication(3, 8));
+            Console.WriteLine(Functions.Division(10, 2));
+            Console.ReadKey();
+        }
+    }
+}

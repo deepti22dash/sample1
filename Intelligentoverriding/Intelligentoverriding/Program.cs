9a542242f33d415a66d7acda0506c8e19242bffd
Deepti <deeptidash22@gmail.com>
2020-09-18 18:03:16 +0530

    overriding1

---
+++ Intelligentoverriding/Intelligentoverriding/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intelligentoverriding
+{
+    class Figure
+    {
+        public virtual void draw()
+        {
+            Console.WriteLine("It is the coding for figure class.");
+        }
+    }
+    class Rectangle : Figure
+    {
+        public override void draw()
+        {
+            Console.WriteLine(" Itis the coding for rectangle class.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure r = new Rectangle();
+            r.draw();
+            Console.ReadKey();
+        }
+    }
+}

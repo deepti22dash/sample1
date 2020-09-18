33c316a96fd9a451d177a0cca0919236e19ad9c2
Deepti <deeptidash22@gmail.com>
2020-09-18 16:02:23 +0530

    webprogram 1

---
+++ ConsoleAppFactorial/ConsoleAppFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClasslibraryFact;
+
+namespace ConsoleAppFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter numbers to calculate");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Factorial f = new Factorial();
+            int res = f.Fact(num);
+
+            Console.WriteLine(" Factorial is " + res);
+            Console.ReadKey();
+        }
+    }
+}

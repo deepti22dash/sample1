0be9ec95bf612412f15623550c6cf56652b4860b
Deepti <deeptidash22@gmail.com>
2020-09-19 13:26:41 +0530

    contructor3

---
+++ useofexpression/useofexpression/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace useofexpression
+{
+    public class Location
+    {
+        private string locname;
+        public Location(string name) => Name = name;
+        public string Name
+        {
+            get => locname;
+            set => locname = value;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Location l = new Location(" Anant");
+            Console.ReadKey();
+        }
+    }
+}

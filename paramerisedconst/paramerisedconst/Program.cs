f277c9942fb82e41cd8fb012b167a154af83a24b
Deepti <deeptidash22@gmail.com>
2020-09-19 13:25:00 +0530

    constructor2

---
+++ paramerisedconst/paramerisedconst/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paramerisedconst
+{
+    public class Person
+    {
+        private string first;
+        private string last;
+        public Person(string firstname, string lastname)
+        {
+            first = firstname;
+            last = lastname;
+        }
+        public void show()
+        {
+            Console.WriteLine("FIRSTNAME " +first);
+            Console.WriteLine(" LASTNAME " +last);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Vibha" , "Dash");
+            p.show();
+            Console.ReadKey(); 
+                
+        }
+    }
+}
+;

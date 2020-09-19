b086e0609ac83b6dff0857834d44cd04bed5dc22
Deepti <deeptidash22@gmail.com>
2020-09-19 13:22:33 +0530

    constructor1

---
+++ constructor1/constructor1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor1
+{
+    class BankAccount
+    {
+        public int actno;
+        public BankAccount()
+        {
+            actno = 22;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+            Console.WriteLine("Account no " + ba.actno);
+            Console.ReadKey(); 
+        }
+    }
+}
